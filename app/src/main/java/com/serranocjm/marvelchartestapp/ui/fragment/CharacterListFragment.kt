47f65549f365e163f8d60f526e4ed28fad318809
C. Serrano <serrano.cjm@gmail.com>
2022-11-23 15:34:19 -0400

    Fragment: - Add viewmodel calls

--- app/src/main/java/com/serranocjm/marvelchartestapp/ui/fragment/CharacterListFragment.kt
+++ app/src/main/java/com/serranocjm/marvelchartestapp/ui/fragment/CharacterListFragment.kt
@@ -1,23 +1,77 @@
 package com.serranocjm.marvelchartestapp.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.serranocjm.marvelchartestapp.R
+import com.serranocjm.marvelchartestapp.data.model.character.Hero
+import com.serranocjm.marvelchartestapp.databinding.FragmentCharacterListBinding
+import com.serranocjm.marvelchartestapp.ui.viewmodel.CharacterViewModel
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-class CharacterListFragment : BaseFragment() {
+class CharacterListFragment : BaseFragment(), KoinComponent {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    // Inject viewmodel
+    private val characterViewModel: CharacterViewModel by inject()
 
+    // Set up fragment binding
+    private var _binding: FragmentCharacterListBinding? = null
+
+    // This property is valid only betweet onCreateView and onDestroy
+    private val binding get() = _binding!!
+
+    // Variables
+    private var offset: Int? = null
+    private var characterList: List<Hero>? = null
+
+    // Fragment functions
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        // Observe viewmodel
+        observeViewModel()
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_character_list, container, false)
+        _binding = FragmentCharacterListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setOffset()
+        loadData()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    // Viewmodel functions
+    private fun observeViewModel() = characterViewModel.run {
+        loadingState.observe(viewLifecycleOwner) {
+            Log.d("TAGTAG", "loading!")
+        }
+        offsetValue.observe(viewLifecycleOwner) {
+            offset = it
+        }
+        heroList.observe(viewLifecycleOwner) {
+            characterList = it
+        }
+    }
+
+    private fun loadData() {
+        offset?.let {
+            characterViewModel.getHeroList(it)
+        }
+    }
+
+    private fun setOffset() {
+        if (offset == null) {
+            characterViewModel.setRequestOffset(0)
+        }
     }
 }
