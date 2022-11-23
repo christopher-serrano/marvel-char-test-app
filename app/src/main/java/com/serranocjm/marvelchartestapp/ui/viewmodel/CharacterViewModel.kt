06b7c1536293426e954a0196a925c906ab885fbd
C. Serrano <serrano.cjm@gmail.com>
2022-11-23 14:29:15 -0400

    Add character (hero) viewmodel

---
+++ app/src/main/java/com/serranocjm/marvelchartestapp/ui/viewmodel/CharacterViewModel.kt
@@ -0,0 +1,63 @@
+package com.serranocjm.marvelchartestapp.ui.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.serranocjm.marvelchartestapp.data.model.character.Hero
+import com.serranocjm.marvelchartestapp.repository.CharacterRepository
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+class CharacterViewModel : BaseViewModel(), KoinComponent {
+
+    // Inject repository directly
+    private val characterRepository: CharacterRepository by inject()
+
+    // LiveData variables
+    val heroList = MutableLiveData<List<Hero>?>()
+    val heroDetail = MutableLiveData<Hero?>()
+
+    // Public functions (to be accessed from the view)
+
+    // Get Hero List
+    fun getHeroList(offset: Int) {
+    }
+
+    // Get Hero etail
+    fun getHeroDetail(id: Int) {
+    }
+
+    // Private suspend functions
+
+    // Async get hero list
+    private suspend fun getHeroListAsync(offset: Int) {
+        val result = kotlin.runCatching {
+            showLoading()
+            characterRepository.getCharacterList(offset)
+        }
+        with(result) {
+            dismissLoading()
+            onSuccess {
+                heroList.postValue(it)
+            }
+            onFailure {
+                onError.postValue(it.message)
+            }
+        }
+    }
+
+    // Async get hero detail
+    private suspend fun getHeroDetailAsync(id: Int) {
+        val result = kotlin.runCatching {
+            showLoading()
+            characterRepository.getCharacterDetail(id)
+        }
+        with(result) {
+            dismissLoading()
+            onSuccess {
+                heroDetail.postValue(it)
+            }
+            onFailure {
+                onError.postValue(it.message)
+            }
+        }
+    }
+}
