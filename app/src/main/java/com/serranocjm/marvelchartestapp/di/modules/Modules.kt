65e93c54c758b3682dfcba688017e8ca99e5a6aa
C. Serrano <serrano.cjm@gmail.com>
2022-11-23 15:05:08 -0400

    DI: - Add modules

--- app/src/main/java/com/serranocjm/marvelchartestapp/di/modules/Modules.kt
+++ app/src/main/java/com/serranocjm/marvelchartestapp/di/modules/Modules.kt
@@ -1,19 +1,32 @@
 package com.serranocjm.marvelchartestapp.di.modules
 
+import com.serranocjm.marvelchartestapp.network.api.ApiClient
+import com.serranocjm.marvelchartestapp.network.interceptor.RequestInterceptor
+import com.serranocjm.marvelchartestapp.network.interceptor.RequestInterceptorImpl
+import com.serranocjm.marvelchartestapp.repository.CharacterRepository
+import com.serranocjm.marvelchartestapp.repository.CharacterRepositoryImpl
+import com.serranocjm.marvelchartestapp.ui.viewmodel.CharacterViewModel
+import com.serranocjm.marvelchartestapp.utils.custom.ImageLoader
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val networkModule = module {
     // Initialize network-related classes
+    single { ApiClient.invoke() }
+    single<RequestInterceptor> { RequestInterceptorImpl() }
 }
 
 val repositoryModule = module {
     // Initialize repository-related classes
+    factory<CharacterRepository> { CharacterRepositoryImpl() }
 }
 
 val viewModelModule = module {
     // Initialize viewmodel-related classes
+    viewModel { CharacterViewModel() }
 }
 
 val utilsModule = module {
     // Initialize utils-related classes
+    factory { ImageLoader(get()) }
 }
