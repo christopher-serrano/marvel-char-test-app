bd8fd5a9de8c1f6cac14fa2dbace895f4d722f7c
C. Serrano <serrano.cjm@gmail.com>
2022-11-24 15:36:29 -0400

    Add dynamic adapter logic

--- app/src/main/java/com/serranocjm/marvelchartestapp/ui/fragment/CharacterListFragment.kt
+++ app/src/main/java/com/serranocjm/marvelchartestapp/ui/fragment/CharacterListFragment.kt
@@ -5,9 +5,15 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.serranocjm.marvelchartestapp.data.model.character.Hero
 import com.serranocjm.marvelchartestapp.databinding.FragmentCharacterListBinding
+import com.serranocjm.marvelchartestapp.ui.adapter.base.DynamicAdapter
+import com.serranocjm.marvelchartestapp.ui.adapter.base.ItemModel
+import com.serranocjm.marvelchartestapp.ui.adapter.item.model.HeroItemModel
+import com.serranocjm.marvelchartestapp.ui.adapter.type.factory.HeroTypeFactoryImpl
 import com.serranocjm.marvelchartestapp.ui.viewmodel.CharacterViewModel
+import com.serranocjm.marvelchartestapp.utils.general.toastShort
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -25,6 +31,8 @@ class CharacterListFragment : BaseFragment(), KoinComponent {
     // Variables
     private var offset: Int? = null
     private var characterList: List<Hero>? = null
+    private var characterItemModelList: List<ItemModel>? = null
+    private var heroListAdapter: DynamicAdapter? = null
 
     // Fragment functions
     override fun onCreateView(
@@ -60,6 +68,9 @@ class CharacterListFragment : BaseFragment(), KoinComponent {
         }
         heroList.observe(viewLifecycleOwner) {
             characterList = it
+            characterList?.let {
+                setUpDynamicAdapter()
+            }
         }
     }
 
@@ -72,6 +83,44 @@ class CharacterListFragment : BaseFragment(), KoinComponent {
     private fun setOffset() {
         if (offset == null) {
             characterViewModel.setRequestOffset(0)
+        }
+    }
+
+    // Dynamic Adapter
+
+    override fun setUpDynamicAdapter() {
+        characterItemModelList = getHeroListForAdapter(characterList)
+        heroListAdapter = getHeroListAdapter(characterItemModelList?.toMutableList(), onItemClick)
+        binding.rvHeroList.adapter = heroListAdapter
+        binding.rvHeroList.layoutManager = LinearLayoutManager(requireActivity())
+    }
+
+    private fun getHeroListAdapter(
+        list: MutableList<ItemModel>?,
+        onItemClick: (ItemModel, String) -> Unit
+    ): DynamicAdapter = DynamicAdapter(
+        typeFactory = HeroTypeFactoryImpl(),
+        items = list?.toMutableList() ?: mutableListOf(),
+        onClick = onItemClick,
+        imageLoader = imageLoader
+    )
+
+    private fun getHeroListForAdapter(list: List<Hero>?): List<ItemModel> {
+        val result = mutableListOf<ItemModel>()
+        list?.forEach { hero ->
+            result.add(HeroItemModel(hero))
+        }
+        return result
+    }
+
+    private var onItemClick: (ItemModel, String) -> Unit = { item, action ->
+        val hero: HeroItemModel = item as HeroItemModel
+
+        when (action) {
+            "goto_hero_detail" -> requireActivity().toastShort("${hero.model.id} - ${hero.model.name}")
+            else -> {
+                Log.d("TAGTAG", "Other action.")
+            }
         }
     }
 }
